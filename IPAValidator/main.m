ca139e7c49d55aac90d54f6a5c26c0e3bd57c651
Pawel Dudek <p@taptera.com>
2013-08-07 09:32:21 +0200

    Initial Commit

---
+++ IPAValidator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  IPAValidator
+//
+//  Created by Pawel Dudek on 8/7/13.
+//  Copyright (c) 2013 Taptera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
